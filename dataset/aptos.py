e8d0ad5c277d4971c626b3b86dc59855f7988dbd
Arctic-Xiangjian <xiangjianhou11@gmail.com>
2023-02-01 23:27:11 +0400

    init

---
+++ dataset/aptos.py
@@ -0,0 +1,51 @@
+import torch
+import os
+import numpy as np
+import pandas as pd
+from torch.utils.data import Dataset
+import cv2
+from torchvision import transforms
+from torchvision import utils
+from PIL import Image
+class APTOS(Dataset):
+    def __init__(self, root_dir, transform=None, train=True, resize_shape=(1024, 1024)):
+        self.root_dir = root_dir
+        self.train = train
+        self.train_image_path = os.path.join(self.root_dir, 'data/train_images')
+        self.test_image_path = os.path.join(self.root_dir, 'data/test_images')
+        self.train_csv_path = os.path.join(self.root_dir, 'data/train.csv')
+        self.test_csv_path = os.path.join(self.root_dir, 'data/test.csv')
+        self.resize_shape = resize_shape
+
+        # Use transforms.Resize and transforms.ToTensor to resize and convert image to tensor
+        self.transform = transforms.Compose([
+            transforms.Resize(self.resize_shape),
+            transforms.ToTensor(),
+        ]) if transform is None else transform
+
+        self.data = None
+        if self.train:
+            train_data = pd.read_csv(self.train_csv_path)
+            train_data['id_code'] = train_data['id_code'].apply(lambda x: x + '.png')
+            train_data['diagnosis'] = train_data['diagnosis'].astype(int)
+            self.data = train_data
+        else:
+            test_data = pd.read_csv(self.test_csv_path)
+            test_data['id_code'] = test_data['id_code'].apply(lambda x: x + '.png')
+            self.data = test_data
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        img_name = os.path.join(self.train_image_path if self.train else self.test_image_path, self.data.iloc[idx, 0])
+        image = self._read_image(img_name)
+        if self.train:
+            label = self.data.iloc[idx, 1]
+            return image, label
+        return image
+
+    def _read_image(self, img_path):
+        image = Image.open(img_path).convert('RGB')
+        image = self.transform(image)
+        return image
---
+++ model/baseline.py
@@ -0,0 +1,43 @@
+import numpy as np
+import torch
+import timm
+import copy
+from torch import nn
+from torchvision.models.feature_extraction import create_feature_extractor
+from torchvision.models.feature_extraction import get_graph_node_names
+import torchvision.models as models
+from torch.nn import functional as F
+
+BACKBONES = {
+    'resnet101',
+    'resnet152',
+    'dense121',
+    'dense161',
+    'dense201',
+    'tf_efficientnet_b0',
+    'tf_efficientnet_b1',
+    'tf_efficientnet_b2',
+    'vit_tiny_patch16_224',
+    'vit_small_patch16_224',
+    'vit_base_patch16_224',
+    'vit_large_patch16_224',
+}
+
+
+class Baseline(nn.Module):
+    def __init__(
+            self,
+            backbone,
+            num_classes,
+            pretrained=False,
+            pretrained_path=None,
+    ):
+        super().__init__()
+        self.backbone = timm.create_model(backbone, pretrained=pretrained,num_classes=num_classes)
+        if pretrained_path is not None:
+            self.backbone.load_state_dict(torch.load(pretrained_path))
+
+    def forward(self, x):
+        x = self.backbone(x)
+        return x
+        
