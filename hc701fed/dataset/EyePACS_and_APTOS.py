d9a6d0771a5d607402b93593396fc6f8dc9f3968
Arctic-Xiangjian <Xiangjianhou11@gmail.com>
2023-02-05 13:47:11 +0400

    fix bug

--- hc701fed/dataset/EyePACS_and_APTOS.py
+++ hc701fed/dataset/EyePACS_and_APTOS.py
@@ -27,16 +27,20 @@ class Eye_APTOS(Dataset):
 
         if self.mode == 'train' or self.mode == 'val':
             data_dir = os.path.join(self.data_dir, 'train')
-            for label in os.listdir(data_dir):
-                for img in os.listdir(os.path.join(data_dir, label)):
-                    self.data.append(os.path.join(data_dir, label, img))
-                    self.labels.append(int(label))
+            for i in os.listdir(self.data_dir):
+                    data = np.load(os.path.join(self.data_dir, i), allow_pickle=True).item()
+                    # Image to 0-1
+                    image_data = data['image'] / data['image'].max()
+                    self.data.append(image_data)
+                    self.labels.append(data['label'])
             self.data, self.val_data, self.labels, self.val_labels = train_test_split(self.data, self.labels, test_size=self.train_val_split, random_state=42)
         elif self.mode == 'test':
             data_dir = os.path.join(self.data_dir, 'test')
-            for img in os.listdir(data_dir):
-                self.data.append(os.path.join(data_dir, img))
-                self.labels.append(0)
+            for i in os.listdir(self.data_dir):
+                data = np.load(os.path.join(self.data_dir, i), allow_pickle=True).item()
+                # Image to 0-1
+                image_data = data['image'] / data['image'].max()
+                self.data.append(image_data)
         else:
             raise ValueError('mode should be train, val or test')
 
